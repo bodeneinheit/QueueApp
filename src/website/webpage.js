a3603c296164c4739fc22ccae6beca37ee98b867
bodeneinheit <78735871+bodeneinheit@users.noreply.github.com>
2023-12-07 12:37:49 +0100

    fixed local version, removed fps for now

--- src/website/webpage.js
+++ src/website/webpage.js
@@ -16,190 +16,189 @@ function initParticles() {
     });
 }
 
-    let socket = new WebSocket("ws://localhost:6969/websocket");
-    let state = true;
-    let queue = [];
-    let whiteListedNames = ["danial", "daniel", "fabi", "falk", "lukas", "maxi", "mert", "niklas", "simon", "tibo"];
+let socket = new WebSocket("ws://localhost:6969/websocket");
+let state = true;
+let queue = [];
+let whiteListedNames = ["danial", "daniel", "fabi", "falk", "lukas", "maxi", "mert", "niklas", "simon", "tibo"];
 
-    socket.onopen = function () {
-        console.log("socket opened")
-    };
+socket.onopen = function () {
+    console.log("socket opened")
+};
 
-    function addToQueue(name) {
-        socket.send(JSON.stringify({
-            type: 0,
-            message: name
-        }));
+function addToQueue(name) {
+    socket.send(JSON.stringify({
+        type: 0,
+        message: name
+    }));
+}
+
+function removeFromQueue(name) {
+    socket.send(JSON.stringify({
+        type: 1,
+        message: name
+    }));
+}
+
+socket.onmessage = function (event) {
+    let receivedData = event.data;
+    if (receivedData !== '') {
+        queue = receivedData.split(",");
+    } else {
+        queue = [];
     }
+    updateList();
+    console.log(queue);
+};
 
-    function removeFromQueue(name) {
-        socket.send(JSON.stringify({
-            type: 1,
-            message: name
-        }));
+socket.onclose = function (event) {
+    if (event.wasClean) {
+        console.log(`Closed cleanly, code=${event.code} reason=${event.reason}`);
+    } else {
+        // e.g. server process killed or network down
+        // event.code is usually 1006 in this case
+        console.log('Connection died ${event.code}');
     }
+};
 
-    socket.onmessage = function (event) {
-        let receivedData = event.data;
-        if (receivedData !== '') {
-            queue = receivedData.split(",");
-        } else {
-            queue = [];
-        }
-        updateList();
-        console.log(queue);
-    };
+socket.onerror = function () {
+    console.log(`error`);
+};
 
-    socket.onclose = function (event) {
-        if (event.wasClean) {
-            console.log(`Closed cleanly, code=${event.code} reason=${event.reason}`);
-        } else {
-            // e.g. server process killed or network down
-            // event.code is usually 1006 in this case
-            console.log('Connection died ${event.code}');
-        }
-    };
-
-    socket.onerror = function () {
-        console.log(`error`);
-    };
-
-    const queueList = document.getElementById('queueList');
-    const inputField = document.getElementById('inputField');
-    const addButton = document.getElementById('addButton');
-    const removeButton = document.getElementById('removeButton');
-    const effectsButton = document.getElementById('effectsButton');
+const queueList = document.getElementById('queueList');
+const inputField = document.getElementById('inputField');
+const addButton = document.getElementById('addButton');
+const removeButton = document.getElementById('removeButton');
+const effectsButton = document.getElementById('effectsButton');
 
 
-    jsEnableElement('addButton');
-    jsDisableElement('removeButton');
-    let savedUser;
-    const useWhiteList = false;
+jsEnableElement('addButton');
+jsDisableElement('removeButton');
+let savedUser;
+const useWhiteList = false;
 
-    addButton.addEventListener('click', function () {
-        const person = inputField.value.trim();
-        if (!useWhiteList || (useWhiteList && whiteListedNames.includes(person.toLowerCase()))) {
-            if (!/^\d+$/.test(person)) {
-                if (person.length < 25) {
-                    if (person !== '') {
-                        if (!queue.includes(person)) {
-                            savedUser = person;
-                            addToQueue(person);
-                            /* for (let i = 0; i < 15; i++) {
-                                 addToQueue(Math.random().toFixed(2));
-                             }*/
-                            if (state) {
-                                state = updateButtons(state);
-                            }
-                        } else {
-                            alert("name is already taken");
+addButton.addEventListener('click', function () {
+    const person = inputField.value.trim();
+    if (!useWhiteList || (useWhiteList && whiteListedNames.includes(person.toLowerCase()))) {
+        if (!/^\d+$/.test(person)) {
+            if (person.length < 25) {
+                if (person !== '') {
+                    if (!queue.includes(person)) {
+                        savedUser = person;
+                        addToQueue(person);
+                        /* for (let i = 0; i < 15; i++) {
+                             addToQueue(Math.random().toFixed(2));
+                         }*/
+                        if (state) {
+                            state = updateButtons(state);
                         }
                     } else {
-                        alert("name may not be empty");
+                        alert("name is already taken");
                     }
                 } else {
-                    alert("name exceeds length limit");
+                    alert("name may not be empty");
                 }
             } else {
-                alert("name must include a non-numeric character");
+                alert("name exceeds length limit");
             }
         } else {
-            alert("name must be whitelisted");
+            alert("name must include a non-numeric character");
         }
-    });
-
-    removeButton.addEventListener('click', function () {
-        removeFromQueue(savedUser);
-        if (!state) {
-            state = updateButtons(state);
-        } else {
-            alert("invalid action");
-        }
-    });
-
-    let enabled = getVisible();
-    let oldValue;
-
-    effectsButton.addEventListener('click', function () {
-        if (enabled) {
-            oldValue = pJSDom[0].pJS.particles.number.value;
-            pJSDom[0].pJS.particles.number.value = 0;
-            pJSDom[0].pJS.fn.particlesRefresh();
-            setVisible(false);
-        } else {
-            if (jsonParticles !== undefined) {
-                pJSDom[0].pJS.particles.number.value = oldValue;
-                pJSDom[0].pJS.fn.particlesRefresh();
-            } else {
-                initParticles();
-            }
-            setVisible(true);
-        }
-        enabled = !enabled;
-    });
-
-    function updateList() {
-        queueList.innerHTML = '';
-
-        if (queue.length === 0) {
-            // Add default entry if the array is empty
-            const defaultItem = document.createElement('div');
-            defaultItem.classList.add("empty");
-            defaultItem.textContent = 'Queue is empty';
-            queueList.appendChild(defaultItem);
-        } else {
-            queue.forEach(function (person, index) {
-                const listItem = document.createElement('li');
-                listItem.textContent = (index + 1) + '. ' + person;
-                queueList.appendChild(listItem);
-
-                setTimeout(function () {
-                    listItem.classList.add("fade-in");
-                }, 10);
-            });
-        }
+    } else {
+        alert("name must be whitelisted");
     }
-
-    function updateButtons(state) {
-        jsDisableElement('removeButton');
-        jsDisableElement('addButton');
-        if (!state) {
-            setTimeout(() => {
-                jsEnableElement('addButton');
-            }, 750)
-        } else {
-            setTimeout(() => {
-                jsEnableElement('removeButton');
-            }, 750)
-        }
-        return !state;
-    }
-
-    function jsEnableElement(id) {
-        if (document.getElementById(id)) {
-            document.getElementById(id).removeAttribute("disabled");
-            document.getElementById(id).className = "enabled";
-            //document.getElementById(id).disabled = false;
-        }
-    }
-
-    function jsDisableElement(id) {
-        if (document.getElementById(id)) {
-            document.getElementById(id).removeAttribute("enabled");
-            document.getElementById(id).className = "disabled";
-            //document.getElementById(id).disabled = true;
-        }
-    }
-
-    setTimeout(() => {
-        document.getElementById("p").innerHTML = "New text!";
-    }, 1000);
-
-
-    setInterval(async () => {
-        let keepActive = await fetch("http://localhost:6969/keepactive");
-    }, 1000 * 60 * 1);
 });
+
+removeButton.addEventListener('click', function () {
+    removeFromQueue(savedUser);
+    if (!state) {
+        state = updateButtons(state);
+    } else {
+        alert("invalid action");
+    }
+});
+
+let enabled = getVisible();
+let oldValue;
+
+effectsButton.addEventListener('click', function () {
+    if (enabled) {
+        oldValue = pJSDom[0].pJS.particles.number.value;
+        pJSDom[0].pJS.particles.number.value = 0;
+        pJSDom[0].pJS.fn.particlesRefresh();
+        setVisible(false);
+    } else {
+        if (jsonParticles !== undefined) {
+            pJSDom[0].pJS.particles.number.value = oldValue;
+            pJSDom[0].pJS.fn.particlesRefresh();
+        } else {
+            initParticles();
+        }
+        setVisible(true);
+    }
+    enabled = !enabled;
+});
+
+function updateList() {
+    queueList.innerHTML = '';
+
+    if (queue.length === 0) {
+        // Add default entry if the array is empty
+        const defaultItem = document.createElement('div');
+        defaultItem.classList.add("empty");
+        defaultItem.textContent = 'Queue is empty';
+        queueList.appendChild(defaultItem);
+    } else {
+        queue.forEach(function (person, index) {
+            const listItem = document.createElement('li');
+            listItem.textContent = (index + 1) + '. ' + person;
+            queueList.appendChild(listItem);
+
+            setTimeout(function () {
+                listItem.classList.add("fade-in");
+            }, 10);
+        });
+    }
+}
+
+function updateButtons(state) {
+    jsDisableElement('removeButton');
+    jsDisableElement('addButton');
+    if (!state) {
+        setTimeout(() => {
+            jsEnableElement('addButton');
+        }, 750)
+    } else {
+        setTimeout(() => {
+            jsEnableElement('removeButton');
+        }, 750)
+    }
+    return !state;
+}
+
+function jsEnableElement(id) {
+    if (document.getElementById(id)) {
+        document.getElementById(id).removeAttribute("disabled");
+        document.getElementById(id).className = "enabled";
+        //document.getElementById(id).disabled = false;
+    }
+}
+
+function jsDisableElement(id) {
+    if (document.getElementById(id)) {
+        document.getElementById(id).removeAttribute("enabled");
+        document.getElementById(id).className = "disabled";
+        //document.getElementById(id).disabled = true;
+    }
+}
+
+setTimeout(() => {
+    document.getElementById("p").innerHTML = "New text!";
+}, 1000);
+
+
+setInterval(async () => {
+    let keepActive = await fetch("http://localhost:6969/keepactive");
+}, 1000 * 60 * 1);
 
 function getVisible() {
     let value = localStorage.getItem('visible');
